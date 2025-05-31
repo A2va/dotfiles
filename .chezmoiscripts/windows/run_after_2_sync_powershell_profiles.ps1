9b77fea4feb257230bd9ccf490efbd2a22325255
A2va <49582555+A2va@users.noreply.github.com>
2025-05-31 18:50:06 +0200

    Basic first powershell profile

---
+++ .chezmoiscripts/windows/run_after_2_sync_powershell_profiles.ps1
@@ -0,0 +1,12 @@
+Set-StrictMode -Version 3.0
+$ErrorActionPreference = "Stop"
+
+$userprofile_documents = "$env:USERPROFILE\Documents"
+$mydocuments = [environment]::getfolderpath("mydocuments")
+
+if (-Not $mydocuments.ToLower().StartsWith($userprofile_documents.ToLower())) {
+    Copy-Item "$userprofile_documents\WindowsPowerShell\profile.ps1" -Destination "$mydocuments\WindowsPowerShell\" -Recurse
+    Copy-Item "$userprofile_documents\WindowsPowerShell\profile.ps1" -Destination "$mydocuments\PowerShell\" -Recurse
+    Copy-Item "$userprofile_documents\WindowsPowerShell\internal.ps1" -Destination "$mydocuments\WindowsPowerShell\" -Recurse
+    Copy-Item "$userprofile_documents\WindowsPowerShell\internal.ps1" -Destination "$mydocuments\PowerShell\" -Recurse
+}
